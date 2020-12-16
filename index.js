3c12f4137e6f31e28eee5ed64ef43f35d10a9ee0
alinaYamkova <alyamkova@yahoo.com>
2020-12-16 14:24:34 +0200

    зберігає готовий варіант рішення

--- index.js
+++ index.js
@@ -6,8 +6,33 @@ const colors = [
   '#009688',
   '#795548',
 ];
-
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-action=start]');
+const stopBtn = document.querySelector('button[data-action=stop]');
+let intervalId = null;
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
+};
+// const idx = randomIntegerFromInterval(1, 6);
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+  
+function onStart() {
+  if (onStop) {
+    clearInterval(intervalId);
+    return;
+  }; 
+
+  intervalId = setInterval(() => {
+    const randomColor = colors[randomIntegerFromInterval(1, 6)];
+    console.log(randomColor);
+    body.style.backgroungColor = randomColor
+  }, 1000);
+};
+
+function onStop() {
+  clearInterval(intervalId);
+  console.log("setInterval stopped!");
 };
